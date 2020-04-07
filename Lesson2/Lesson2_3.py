26b12fedcef4b1e8d988e97be36fa8a5d2c63988
undrey <mail@undrey.com>
2020-04-07 08:37:57 +0300

    переделал на рекурсию

--- Lesson2/Lesson2_3.py
+++ Lesson2/Lesson2_3.py
@@ -1,10 +1,18 @@
 # Сформировать из введенного числа обратное по порядку входящих в него цифр и вывести на
 # экран. Например, если введено число 3486, то надо вывести число 6843
 
+
 n = int(input("Введите число: "))
 new_n = ''
-while n > 0.9:
-    i = n % 10
-    new_n += str(i)
-    n = int(n / 10)
+
+
+def find_new_n(input_n):
+    global new_n
+    if input_n > 0.9:
+        i = input_n % 10
+        new_n += str(i)
+        find_new_n(int(input_n / 10))
+
+
+find_new_n(n)
 print("Обратное число - ", new_n)
