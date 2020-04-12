df6f25146622a241f949770cdf1c21710a68f2a4
undrey <mail@undrey.com>
2020-04-12 15:10:25 +0300

    Бонусом

--- Lesson3/Lesson3_7.py
+++ Lesson3/Lesson3_7.py
@@ -3,18 +3,30 @@
 import random
 
 SIZE = 10
-MIN_ITEM = -10
-MAX_ITEM = 10
+MIN_ITEM = 0
+MAX_ITEM = 100
 array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
 print(array)
 
 min_ = array[0]
-for spam in array:
-    if spam < min_:
-        min_ = spam
-print(min_)
+pos_min = 0
+for spam in range(0, len(array)):
+    if array[spam] < min_:
+        min_ = array[spam]
+        pos_min = spam
+print(f'в данном массиве минимальное {min_} в позиции {pos_min}')
 
-for eggs in array:
-    if eggs < min_:
-        min_ = eggs
-print(min_)
+if pos_min == 0:
+    min2_ = array[1]
+    pos_min2 = 1
+else:
+    min2_ = array[0]
+    pos_min2 = 0
+
+for spam in range(0, len(array)):
+    if spam != pos_min:
+        if array[spam] < min2_:
+            min2_ = array[spam]
+            pos_min2 = spam
+
+print(f'второе минимальное {min2_} в позиции {pos_min2}')
