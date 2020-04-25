bcb94b4ff17b7e35133cdc7b8992139058f2f3e0
undrey <mail@undrey.com>
2020-04-25 18:04:00 +0300

    ДЗ_7

--- Lesson7/Lesson7_1.py
+++ Lesson7/Lesson7_1.py
@@ -3,3 +3,20 @@
 # Примечания:
 # ● алгоритм сортировки должен быть в виде функции, которая принимает на вход массив данных,
 # ● постарайтесь сделать алгоритм умнее, но помните, что у вас должна остаться сортировка пузырьком.
+import random
+
+
+def stone_sort(array):
+    n = 1
+    while n < len(array):
+        for i in range(len(array) - 1):
+            if array[i] < array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
+        n += 1
+        print(array)
+
+
+my_array = [random.randrange(-100, 99) for i in range(10)]
+print(my_array)
+stone_sort(my_array)
+print(my_array)
--- Lesson7/Lesson7_2.py
+++ Lesson7/Lesson7_2.py
@@ -1,2 +1,27 @@
 # Отсортируйте по возрастанию методом слияния одномерный вещественный массив, заданный случайными числами
 # на промежутке [0; 50). Выведите на экран исходный и отсортированный массивы.
+
+import random
+
+def res_array(left_ar,right_ar):
+    result=[]
+
+    return result
+
+
+def merge_sort(array):
+    n = len(array)
+    if n > 1:
+        middle = int(len(array) / 2)
+        left_ = merge_sort(array[:middle])
+        right_ = merge_sort(array[middle:])
+        return res_array(left_, right_)
+    else:
+        return array
+
+
+# my_array = [random.random() * 50 for i in range(10)]
+my_array = [random.randrange(-100, 99) for i in range(10)]
+print(my_array)
+merge_sort(my_array)
+print(my_array)
